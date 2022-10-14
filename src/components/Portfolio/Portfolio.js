7ce0a50723b33316324146932850075b78d7d0ef
Vladimir Iakhnev <Yahnev91@yandex.ru>
2022-10-14 10:50:25 +0300

    Диплом: начало

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,5 +1,4 @@
 import './Portfolio.css';
-import link from '../../images/link.png'
 
 function Portfolio() {
 
@@ -11,21 +10,15 @@ function Portfolio() {
         <ul className="portfolio__list">
             <li className="portfolio__item">
                 <p className="portfolio__item-text">Статичный сайт</p>
-                <a className="portfolio__item-link" href='https://github.com/YVA91/react-mesto-api-full' target="_blank">
-                    <img src={link} className='portfolio__arrow' alt='стрелка'/>
-                </a>
+                <a className="portfolio__item-link" href='https://github.com/YVA91/react-mesto-api-full' target="_blank">↗</a>
             </li>
             <li className="portfolio__item">
                 <p className="portfolio__item-text">Адаптивный сайт</p>
-                <a className="portfolio__item-link" href='https://github.com/YVA91/russian-travel-Iakhnev' target="_blank">
-                    <img src={link} className='portfolio__arrow' alt='стрелка'/>
-                </a>
+                <a className="portfolio__item-link" href='https://github.com/YVA91/russian-travel-Iakhnev' target="_blank">↗</a>
             </li>
             <li className="portfolio__item">
                 <p className="portfolio__item-text">Одностраничное приложение</p>
-                <a className="portfolio__item-link" href='https://github.com/YVA91/how-to-learn' target="_blank">
-                    <img src={link} className='portfolio__arrow' alt='стрелка'/>
-                </a>
+                <a className="portfolio__item-link" href='https://github.com/YVA91/how-to-learn' target="_blank">↗</a>
             </li>
         </ul>
     </section>
