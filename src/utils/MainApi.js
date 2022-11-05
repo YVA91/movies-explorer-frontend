6b3027b4deca320aa9ce240e99c2917fa5ebab31
Vladimir Iakhnev <Yahnev91@yandex.ru>
2022-11-05 14:43:10 +0300

    Диплом

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,4 +1,4 @@
-const BASE_URL = /*'https://api.yva.diploma.nomoredomains.icu'*/  'http://localhost:3001';
+const BASE_URL = 'https://api.yva.diploma.nomoredomains.icu';
 
 const report = (res) => {
   if (res.ok) {
